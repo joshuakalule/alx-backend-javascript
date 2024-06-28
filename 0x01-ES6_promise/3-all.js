61f093b604b70d0b57e77d6ce3f317222fd1956f
Kalule Joshua <kaljoshkalule@gmail.com>
2024-06-28 21:52:38 +0000

    add file 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,8 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((resp) => {
+      console.log(`${resp[0].body} ${resp[1].firstName} ${resp[1].lastName}`);
+    });
+}
