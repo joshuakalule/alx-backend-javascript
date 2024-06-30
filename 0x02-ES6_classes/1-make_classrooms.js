abba8c6ec3ffdda4d71b7306a62aa288265cce66
Kalule Joshua <kaljoshkalule@gmail.com>
2024-06-30 10:43:53 +0000

    add semicolons

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,7 +1,7 @@
-import Classrom from './0-classroom'
+import Classrom from './0-classroom';
 
 export default function initializeRooms() {
   return [
     new Classrom(19), new Classrom(20), new Classrom(34),
-  ]
+  ];
 }
