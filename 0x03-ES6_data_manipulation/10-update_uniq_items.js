0e04145734f93cd507ce6b68294e7b7762409246
Kalule Joshua <kaljoshkalule@gmail.com>
2024-07-02 23:37:04 +0000

    update file 10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,7 +1,8 @@
-export default function updateUniqueItems(mapObj) {
-  return mapObj.forEach((value, key, map) => {
-    if (value === 1) {
-      map.set(key, 100);
-    }
-  });
+export default function updateUniqueItems(map) {
+  const array = Array.from(map).filter((x) => x[1] === 1);
+  if (array.length === 0) {
+    throw new Error('Cannot process');
+  }
+  array.forEach((x) => map.set(x[0], 100));
+  return map;
 }
