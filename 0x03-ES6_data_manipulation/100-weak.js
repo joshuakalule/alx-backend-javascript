bbd9cec20c8a1d9911a9f1f829626871055dd7e3
Kalule Joshua <kaljoshkalule@gmail.com>
2024-07-02 23:46:12 +0000

    add file 100

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,1 +1,9 @@
 export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  const count = weakMap.get(endpoint) || 0;
+  weakMap.set(endpoint, count + 1);
+  if (count >= 4) {
+    throw new Error('Endpoint load is high');
+  }
+}
