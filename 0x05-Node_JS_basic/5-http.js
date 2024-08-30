88937fcd09448e04571944e987bcaa45b2039212
Joshua Kalule <kaljoshkalule@gmail.com>
2024-08-30 17:00:01 +0300

    add file 5

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,63 @@
+const http = require('http');
+const fs = require('fs');
+
+const filepath = process.argv.length > 2 ? process.argv[2] : 'nope.csv';
+
+const countStudents = (filepath) => new Promise((resolve, reject) => {
+  fs.readFile(filepath, 'utf-8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+    }
+    if (data) {
+      const lines = data.trim().split('\n').map((line) => line.replace('\r', ''));
+
+      const n = lines.length - 1;
+
+      const fields = {};
+      for (let i = 1; i < lines.length; i += 1) {
+        const firstname = lines[i].split(',')[0];
+        const field = lines[i].split(',').pop();
+        if (field in fields) {
+          fields[field].push(firstname);
+        } else {
+          fields[field] = [firstname];
+        }
+      }
+
+      let text = `Number of students: ${n}\n`;
+
+      for (const [field, names] of Object.entries(fields)) {
+        const len = names.length;
+        const list = names.join(', ');
+        text += `Number of students in ${field}: ${len}. List: ${list}\n`;
+      }
+      resolve(text);
+    }
+  });
+});
+
+const app = http.createServer(async (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.statusCode = 200;
+  const { url } = req;
+
+  if (url === '/') {
+    res.write('Hello Holberton School!\n');
+  } else if (url === '/students') {
+    try {
+      const studentData = await countStudents(filepath);
+      res.write('This is the list of our students\n');
+      res.write(studentData);
+    } catch (err) {
+      res.write('Internal Server Error\n');
+      res.statusCode = 500;
+    }
+  }
+  res.end();
+});
+
+app.listen(1245, () => {
+  console.log('Server running at http://localhost:1245');
+});
+
+module.exports = app;
