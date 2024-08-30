f05b6455678f1e1a2d341161907d0c790160d619
Joshua Kalule <kaljoshkalule@gmail.com>
2024-08-30 15:43:48 +0300

    set response code after setting headers

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -5,9 +5,9 @@ const host = 'localhost';
 
 const app = http.createServer((req, res) => {
   const text = 'Hello Holberton School!';
-  res.writeHead(200);
   res.setHeader('Content-Type', 'text/plain');
   res.setHeader('Content-Length', text.length);
+  res.writeHead(200);
   res.end(text);
 });
 
