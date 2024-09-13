b947602fd918896d843031c35d6d3f808c374701
Joshua Kalule <kaljoshkalule@gmail.com>
2024-09-13 16:44:30 +0300

    remove spy tests

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -31,33 +31,4 @@ describe('sendPaymentRequestToApi', function () {
       expect(this.consoleLogSpy.calledWith('The total is: 10')).to.be.true;
     });
   });
-
-  describe('using spies', function () {
-    beforeEach(function () {
-      this.fnSpy = sinon.spy(Utils, 'calculateNumber');
-      this.consoleLogSpy = sinon.stub(console, 'log');
-      this.a = 10.2;
-      this.b = 4.8;
-
-      sendPaymentRequestToApi(this.a, this.b);
-      this.expectedResult = Math.round(this.a) + Math.round(this.b);
-    });
-
-    afterEach(function () {
-      this.fnSpy.restore();
-      this.consoleLogSpy.restore();
-    });
-
-    it('should call calculateNumber', function () {
-      expect(this.fnSpy.called).to.be.true;
-    });
-
-    it('should call calculateNumber with right args', function () {
-      expect(this.fnSpy.calledWith('SUM', this.a, this.b)).to.be.true;
-    });
-
-    it('should call calculateNumber and return correctly', function () {
-      expect(this.fnSpy.returned(this.expectedResult)).to.be.true;
-    });
-  });
 });
