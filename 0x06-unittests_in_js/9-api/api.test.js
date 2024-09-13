39cc3edb1e8d7c4c807ae362452d9054d5bd4047
Joshua Kalule <kaljoshkalule@gmail.com>
2024-09-14 01:43:55 +0300

    add test for GET /

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -4,26 +4,34 @@ const { expect } = require('chai');
 describe('API index page', function() {
   const URL = 'http://localhost:7865';
 
-  it('tests GET /cart/:id when id is number', function (done) {
-    const id = 23;
-    request.get(`${URL}/cart/${id}`, (err, res, body) => {
+  it('tests GET /', function (done) {
+    request.get(`${URL}`, (err, res, body) => {
       if (err) {
         done(new Error('error in request'));
       }
       expect(res.statusCode).to.be.equal(200);
-      expect(body).to.be.equal(`Payment methods for cart ${id}`);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('tests GET /cart/:id when id is number', function (done) {
+    request.get(`${URL}/cart/23`, (err, res, body) => {
+      if (err) {
+        done(new Error('error in request'));
+      }
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal(`Payment methods for cart 23`);
       done();
     });
   });
 
   it('tests GET /cart/:id when id is not a number', function (done) {
-    const id = 'NaN';
-    request.get(`${URL}/cart/${id}`, (err, res, body) => {
+    request.get(`${URL}/cart/NaN`, (err, res, body) => {
       if (err) {
         done(new Error('error in request'));
       }
       expect(res.statusCode).to.not.be.equal(200);
-      expect(body).to.not.be.equal(`Payment methods for cart ${id}`);
       done();
     });
   });
