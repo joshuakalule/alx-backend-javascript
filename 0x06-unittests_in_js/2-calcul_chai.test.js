f620ed1d8313ff402f7579413be0fe4bd0d87e35
Joshua Kalule <kaljoshkalule@gmail.com>
2024-09-13 03:22:58 +0300

    add type as string not variable in method

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -5,63 +5,60 @@ const expect = chai.expect;
 
 describe('tests the method calculateNumber', function () {
   describe('test when type is SUM', function () {
-    const type = 'SUM';
     it('tests a rounded', function () {
-      const test1 = calculateNumber(type, 2.2, 3);
+      const test1 = calculateNumber('SUM', 2.2, 3);
       expect(test1).to.be.equal(5);
     });
 
     it('tests b rounded', function () {
-      const test1 = calculateNumber(type, 2, 3.1);
+      const test1 = calculateNumber('SUM', 2, 3.1);
       expect(test1).to.be.equal(5);
     });
 
     it('tests both rounded', function () {
-      const test1 = calculateNumber(type, 2.2, 3.1);
+      const test1 = calculateNumber('SUM', 2.2, 3.1);
       expect(test1).to.be.equal(5);
     });
   });
 
   describe('test when type is SUBTRACT', function () {
-    const type = 'SUBTRACT';
     it('tests a rounded', function () {
-      const test1 = calculateNumber(type, 2.2, 3);
+      const test1 = calculateNumber('SUBTRACT', 2.2, 3);
       expect(test1).to.be.equal(-1);
     });
 
     it('tests b rounded', function () {
-      const test1 = calculateNumber(type, 2, 3.1);
+      const test1 = calculateNumber('SUBTRACT', 2, 3.1);
       expect(test1).to.be.equal(-1);
     });
 
     it('tests both rounded', function () {
-      const test1 = calculateNumber(type, 2.2, 3.1);
+      const test1 = calculateNumber('SUBTRACT', 2.2, 3.1);
       expect(test1).to.be.equal(-1);
     });
   });
 
   describe('test when type is DIVIDE', function () {
-    const type = 'DIVIDE';
     it('tests a rounded', function () {
-      const test1 = calculateNumber(type, 10.2, 2);
+      const test1 = calculateNumber('DIVIDE', 10.2, 2);
       expect(test1).to.be.equal(5);
     });
 
     it('tests b rounded', function () {
-      const test1 = calculateNumber(type, 10, 2.3);
+      const test1 = calculateNumber('DIVIDE', 10, 2.3);
       expect(test1).to.be.equal(5);
     });
 
     it('tests both rounded', function () {
-      const test1 = calculateNumber(type, 10.2, 1.8);
+      const test1 = calculateNumber('DIVIDE', 10.2, 1.8);
       expect(test1).to.be.equal(5);
     });
 
     it('tests when b rounds to zero', function () {
-      expect(calculateNumber(type, 10.2, 0.3)).to.be.equal('Error');
-      expect(calculateNumber(type, 10.2, -0.2)).to.be.equal('Error');
-      expect(calculateNumber(type, 10.2, 0)).to.be.equal('Error');
-      expect(calculateNumber(type, 10.2, -0)).to.be.equal('Error');
+      expect(calculateNumber('DIVIDE', 10.2, 0.3)).to.be.equal('Error');
+      expect(calculateNumber('DIVIDE', 10.2, -0.2)).to.be.equal('Error');
+      expect(calculateNumber('DIVIDE', 10.2, 0)).to.be.equal('Error');
+      expect(calculateNumber('DIVIDE', 10.2, -0)).to.be.equal('Error');
     });
   });
 });
