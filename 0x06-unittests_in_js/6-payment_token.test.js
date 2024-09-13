557668b5015b12727b24dab1a33ea56c378775a1
Joshua Kalule <kaljoshkalule@gmail.com>
2024-09-13 20:10:53 +0300

    rewrite test

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -2,11 +2,12 @@ const getPaymentTokenFromAPI = require('./6-payment_token');
 const { expect } = require('chai');
 
 describe('getPaymentTokenFromAPI', function () {
-  it('should test the result when passed with arg == true', function () {
-    getPaymentTokenFromAPI(true)
-    .then((response) => {
-      expect(response).to.deep.equal({ data: 'Sucessful responsefrom the API' });
-      done();
-    });
+  it('should return a resolved promise with the correct data when success is true', function (done) {
+    getPaymentTokenFromAPI(false)
+      .then(response => {
+        expect(response).to.deep.equal({ data: 'Successful response from the API' });
+        done();
+      })
+      .catch(done);
   });
 });
