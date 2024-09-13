80243bb11f110e033d2277fa327f1c5cd798b7bb
Joshua Kalule <kaljoshkalule@gmail.com>
2024-09-13 03:28:39 +0300

    replace to.be with to

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -7,58 +7,58 @@ describe('tests the method calculateNumber', function () {
   describe('type == "SUM"', function () {
     it('tests a rounded', function () {
       const test1 = calculateNumber('SUM', 2.2, 3);
-      expect(test1).to.be.equal(5);
+      expect(test1).to.equal(5);
     });
 
     it('tests b rounded', function () {
       const test1 = calculateNumber('SUM', 2, 3.1);
-      expect(test1).to.be.equal(5);
+      expect(test1).to.equal(5);
     });
 
     it('tests both rounded', function () {
       const test1 = calculateNumber('SUM', 2.2, 3.1);
-      expect(test1).to.be.equal(5);
+      expect(test1).to.equal(5);
     });
   });
 
   describe('test when type is SUBTRACT', function () {
     it('tests a rounded', function () {
       const test1 = calculateNumber('SUBTRACT', 2.2, 3);
-      expect(test1).to.be.equal(-1);
+      expect(test1).to.equal(-1);
     });
 
     it('tests b rounded', function () {
       const test1 = calculateNumber('SUBTRACT', 2, 3.1);
-      expect(test1).to.be.equal(-1);
+      expect(test1).to.equal(-1);
     });
 
     it('tests both rounded', function () {
       const test1 = calculateNumber('SUBTRACT', 2.2, 3.1);
-      expect(test1).to.be.equal(-1);
+      expect(test1).to.equal(-1);
     });
   });
 
   describe('test when type is DIVIDE', function () {
     it('tests a rounded', function () {
       const test1 = calculateNumber('DIVIDE', 10.2, 2);
-      expect(test1).to.be.equal(5);
+      expect(test1).to.equal(5);
     });
 
     it('tests b rounded', function () {
       const test1 = calculateNumber('DIVIDE', 10, 2.3);
-      expect(test1).to.be.equal(5);
+      expect(test1).to.equal(5);
     });
 
     it('tests both rounded', function () {
       const test1 = calculateNumber('DIVIDE', 10.2, 1.8);
-      expect(test1).to.be.equal(5);
+      expect(test1).to.equal(5);
     });
 
     it('tests when b rounds to zero', function () {
-      expect(calculateNumber('DIVIDE', 10.2, 0.3)).to.be.equal('Error');
-      expect(calculateNumber('DIVIDE', 10.2, -0.2)).to.be.equal('Error');
-      expect(calculateNumber('DIVIDE', 10.2, 0)).to.be.equal('Error');
-      expect(calculateNumber('DIVIDE', 10.2, -0)).to.be.equal('Error');
+      expect(calculateNumber('DIVIDE', 10.2, 0.3)).to.equal('Error');
+      expect(calculateNumber('DIVIDE', 10.2, -0.2)).to.equal('Error');
+      expect(calculateNumber('DIVIDE', 10.2, 0)).to.equal('Error');
+      expect(calculateNumber('DIVIDE', 10.2, -0)).to.equal('Error');
     });
   });
 });
